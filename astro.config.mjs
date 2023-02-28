6058a1c652a90049cc05f7ef0fa2d98b236cd0d7
mic-v <michaelvictorino@gmail.com>
2023-02-27 23:28:37 -0500

    testing website deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mic-v.github.io',
 	integrations: [mdx(), tailwind()],
+	site: 'https://mic-v.github.io',
 });
