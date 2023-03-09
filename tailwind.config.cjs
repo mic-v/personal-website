b7837f5ad9bbe03059cfd95f43bea858db78320d
mic-v <michaelvictorino@gmail.com>
2023-03-09 00:36:16 -0500

    Fixed pathing, fixing about me page, fixed containers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,5 +17,25 @@ module.exports = {
 			}, 
 		}
 	},
-	plugins: [],
+	plugins: [
+		function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1080px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    }
+  ],
 };
