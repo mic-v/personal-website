c44b6f6726b57c15443bf244efbee783da207a1d
mic-v <michaelvictorino@gmail.com>
2023-03-10 15:30:18 -0500

    Changed url of ghostClient

--- frontend/src/lib/ghost.ts
+++ frontend/src/lib/ghost.ts
@@ -2,7 +2,7 @@ import GhostContentAPI from '@tryghost/content-api';
 
 // Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-    url: 'http://localhost:2368', // This is the default URL if your site is running on a local environment
+    url: 'http://127.0.0.1:2368', // This is the default URL if your site is running on a local environment
     key: '9776631b76782ddf161e8c0178',
     version: 'v5.0',
 });
