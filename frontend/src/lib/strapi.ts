4b7b73c499946b4dbe2f576bba7f3f90504d770c
mic-v <michaelvictorino@gmail.com>
2023-03-15 00:33:51 -0400

    readded strapi lines

--- frontend/src/lib/strapi.ts
+++ frontend/src/lib/strapi.ts
@@ -1,4 +1,2 @@
 
-/*
 export const strapiClient = await fetch(`${import.meta.env.URL}`, { headers: { Authorization: '${import.meta.env.AUTHORIZATION} ${import.meta.env.STRAPI_API_KEY'}}).then(res => res.json());
-*/
