e62bc3fd58f21d9c3aac176a5808166ea6abcd51
mic-v <michaelvictorino@gmail.com>
2023-03-15 16:13:37 -0400

    Fixing contact page

--- my-project/config/env/production/database.js
+++ my-project/config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', 'containers-us-west-77.railway.app'),
+      host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 6424),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
-      password: env('PGPASSWORD', 'PMKmxMt6haAV0LYNl93x'),
+      password: env('PGPASSWORD', 'password'),
       ssl: env.bool(true),
     },
   },
