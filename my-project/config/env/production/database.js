9af37b210bab653818994507d7875366edc7119b
mic-v <michaelvictorino@gmail.com>
2023-03-17 01:06:22 -0400

    added more stuff to strapi production

--- my-project/config/env/production/database.js
+++ my-project/config/env/production/database.js
@@ -1,6 +1,22 @@
-module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-  dirs: {
-    public: "/data/public"
-  },
-});
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
+    },
+  }
+};
+
+
