42ffa490981347eef5f4c411b90d21f878768dbb
mic-v <michaelvictorino@gmail.com>
2023-03-17 01:44:00 -0400

    deleted some strapi leftovers

--- frontend/src/lib/strapi.ts
+++
@@ -1,2 +0,0 @@
-
-export const strapiClient = await fetch(`${import.meta.env.URL}`, { headers: { Authorization: '${import.meta.env.AUTHORIZATION} ${import.meta.env.STRAPI_API_KEY'}}).then(res => res.json());
